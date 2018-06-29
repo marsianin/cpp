36ef2cfcc0d4f238cda3eb4ad45e165d8366bb55
rinat <marsianin86@gmail.com>
2018-06-29 09:47:51 +0700

    added comparation and to scalar type conversion

--- Rational.cpp
+++ Rational.cpp
@@ -106,6 +106,17 @@ struct Rational {
         return Rational(numerator_ * r.denominator_, denominator_*r.numerator_);
     }
 
+    int getNumerator() const {
+        return numerator_;
+    }
+
+    int getDenominator() const {
+        return denominator_;
+    }
+
+    operator double() const{
+        return to_double();
+    }
 private:
     int numerator_;
     int denominator_;
@@ -142,26 +153,120 @@ Rational operator /(int i, Rational const r) {
     return Rational(i)/r;
 }
 
+/*
+ * Comparasion
+ */
+bool operator !=(Rational const &r1, Rational const &r2) {
+    return r1.getNumerator()*r2.getDenominator() != r2.getNumerator()*r1.getDenominator();
+}
+
+bool operator ==(Rational const &r1, Rational const &r2) {
+    return r1.getNumerator()*r2.getDenominator() == r2.getNumerator()*r1.getDenominator();
+}
+
+bool operator !=(int i, Rational const &r) {
+    return i*r.getDenominator() != r.getNumerator();
+}
+
+bool operator ==(int i, Rational const &r) {
+    return i*r.getDenominator() == r.getNumerator();
+}
+
+bool operator ==(Rational const &r, int i) {
+    return i == r;
+}
+
+bool operator !=(Rational const &r, int i) {
+    return i != r;
+}
+
+bool operator >(Rational const &r1, Rational const &r2) {
+    return r1.getNumerator()*r2.getDenominator() > r2.getNumerator()*r1.getDenominator();
+}
+
+bool operator <(Rational const &r1, Rational const &r2) {
+    return r1.getNumerator()*r2.getDenominator() < r2.getNumerator()*r1.getDenominator();
+}
+
+bool operator <(Rational const &r, int i) {
+    return r.getNumerator() < i*r.getDenominator();
+}
+
+bool operator <(int i, Rational const &r) {
+    return i*r.getDenominator() < r.getNumerator();
+}
+
+bool operator >=(Rational const &r1, Rational const &r2) {
+    return r1.getNumerator()*r2.getDenominator() >= r2.getNumerator()*r1.getDenominator();
+}
+
+bool operator <=(Rational const &r1, Rational const &r2) {
+    return r1.getNumerator()*r2.getDenominator() <= r2.getNumerator()*r1.getDenominator();
+}
+
+bool operator >(int i, Rational const &r) {
+    return i*r.getDenominator() > r.getNumerator();
+}
+
+bool operator >(Rational const &r, int i) {
+    return r.getNumerator()>i*r.getDenominator();
+}
+
+bool operator >=(int i, Rational const &r) {
+    return i*r.getDenominator() >= r.getNumerator();
+}
+
+bool operator >=(Rational const &r, int i) {
+    return r.getNumerator() >= i*r.getDenominator();
+}
+
+bool operator <=(int i, Rational const &r) {
+    return i*r.getDenominator() <= r.getNumerator();
+}
+
+bool operator <=(Rational const &r, int i) {
+    return r.getNumerator() <= i*r.getDenominator();
+}
+
 int main() {
     Rational r1(1, 2);
     Rational r2(1, 3);
     Rational r3(5);
     Rational r5(575);
 
-    r1.add(r2);
-    std::cout << r1.to_double() << std::endl;
-    r1.sub(r2);
-    std::cout << r1.to_double() << std::endl;
-    r1.neg();
-    std::cout << r1.to_double() << std::endl;
-    r3.mul(r1);
-    std::cout << r3.to_double() << std::endl;
-    r3.div(r2);
-    std::cout << r3.to_double() << std::endl;
+    std::cout << "Start" << std::endl;
 
-    +r5;
-    std::cout << r5.to_double() << std::endl;
+    if (r2<r2){
+        std::cout << "Rational r2(1, 3) < Rational r1(1, 2)" << std::endl;
+    }
 
-    r3 += r5;
-    std::cout << r3.to_double() << std::endl;
+    if (r3<7){
+        std::cout << "Rational r3(5) < 7" << std::endl;
+    }
+
+    if (1<r3){
+        std::cout << "1 < Rational r3(5)" << std::endl;
+    }
+
+    std::cout << "End" << std::endl;
+
+
+
+
+//    r1.add(r2);
+//    std::cout << r1.to_double() << std::endl;
+//    r1.sub(r2);
+//    std::cout << r1.to_double() << std::endl;
+//    r1.neg();
+//    std::cout << r1.to_double() << std::endl;
+//    r3.mul(r1);
+//    std::cout << r3.to_double() << std::endl;
+//    r3.div(r2);
+//    std::cout << r3.to_double() << std::endl;
+//
+//    +r5;
+//    std::cout << r5.to_double() << std::endl;
+//
+//    r3 += r5;
+//    std::cout << r3.to_double() << std::endl;
 }
