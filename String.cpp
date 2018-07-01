b9622cff4ece7a97e4be0ace39ddd1ef4b20a61a
rinat <marsianin86@gmail.com>
2018-07-01 18:37:54 +0700

    Overload [][] operator and memory leak resolve

--- String.cpp
+++ String.cpp
@@ -1,7 +1,11 @@
+#include <iostream>
 #include <cstddef> // size_t
 #include <cstring> // strlen, strcpy
+#include <string>
 
 using namespace std;
+
+struct StringInner;
 
 struct String {
     String(const char *str = ""){
@@ -20,6 +24,29 @@ struct String {
          str[n] = '\0';
     } 
 
+    String(const String &other) : 
+        size(other.size), str(new char[size+1])
+    {
+        for(size_t i = 0; i != size; ++i){
+            str[i] = other.str[i];
+        }
+        
+        str[size] = '\0';
+    }
+
+    String &operator=(const String &other) {
+        if (this == &other) {
+            return *this;
+        }
+        
+        delete [] str;
+        size = other.size;
+        str = new char[size+1];
+        strcpy(str, other.str);
+        
+        return *this;
+    }
+
     void append(String &other)
     {
        size_t prevSize = size;
@@ -33,6 +60,8 @@ struct String {
        str = tmp;
     }
 
+    StringInner operator[] (int k) const;
+
     /* и деструктор */
     ~String() {
         delete [] str;
@@ -42,11 +71,68 @@ struct String {
     char *str;
 };
 
+class StringInner {
+    public:
+
+    StringInner(const char *str = "", size_t start = 0){
+        size = strlen (str);
+        this->start = start;
+        this->str = new char[size+1];
+        strcpy(this->str, str);
+    }
+
+    String operator[] (int k) const {
+        char* tmp = new char[k-start];
+
+        for (unsigned i = 0; i < k-start; i++){
+            tmp[i] = str[i];
+        }
+
+        tmp[k-start] = '\0';
+        String s(tmp);
+        delete [] tmp;
+
+        return s;
+    }
+
+    /* и деструктор */
+    ~StringInner() {
+        delete [] str;
+    }
+
+    size_t size;
+    size_t start;
+    char *str;
+};
+
+StringInner String::operator[] (int k) const {
+    char* tmp = new char[size-k+1];
+
+    for (unsigned i = k; i < size; i++){
+        tmp[i-k] = str[i];
+    }
+
+    tmp[size-k] = '\0';
+
+    StringInner s(tmp, k);
+    delete [] tmp;
+
+    return s;
+}
+
 int main(){
     String s1("Hello,");
     String s2(" world!");
 
+    String s123("hello");
+
+    // Copy three characters of s1 (starting
+    // from position 1)
+    cout << "===" <<  s123[0][4].str << "===" << s123[0][4].size << endl;
+    cout << "===" <<  s123[1][4].str << "===" << s123[1][4].size << endl;
+    cout << "===" <<  s123[5][5].str << "===" << s123[5][5].size << endl;
+
     s1.append(s2);
-    
+
     cout << s1.str << endl;
 }
